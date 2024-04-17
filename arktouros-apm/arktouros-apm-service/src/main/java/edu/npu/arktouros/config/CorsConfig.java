44fda13792b934746a1ee088b6f577e1b3694a77
WangMinan <wangminan0811@hotmail.com>
2024-04-17 21:15:47 +0800

    【修正】跨域

---
+++ arktouros-apm/arktouros-apm-service/src/main/java/edu/npu/arktouros/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package edu.npu.arktouros.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author : [wangminan]
+ * @description : 跨域配置
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 对所有路径生效
+                .allowedOrigins("*") //允许所有源地址
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的请求方法
+                .allowedHeaders("*"); // 允许的请求头
+    }
+}
