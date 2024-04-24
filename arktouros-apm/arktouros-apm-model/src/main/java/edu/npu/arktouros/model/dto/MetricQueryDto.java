7c6e9adb87d98acea883ea25b0d43c58832a316b
WangMinan <wangminan0811@hotmail.com>
2024-04-24 21:23:54 +0800

    修正条件

--- arktouros-apm/arktouros-apm-model/src/main/java/edu/npu/arktouros/model/dto/MetricQueryDto.java
+++ arktouros-apm/arktouros-apm-model/src/main/java/edu/npu/arktouros/model/dto/MetricQueryDto.java
@@ -3,7 +3,6 @@ package edu.npu.arktouros.model.dto;
 import jakarta.validation.constraints.NotEmpty;
 
 public record MetricQueryDto(
-        @NotEmpty(message = "serviceName should not be empty")
         String serviceName,
         Integer metricNameLimit,
         Long startTimeStamp,
