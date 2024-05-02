735f80ba8da17b5479267b97c647b70b8681cd9e
WangMinan <wangminan0811@hotmail.com>
2024-05-02 19:46:53 +0800

    【修正】更新部署脚本

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 docker pull wangminan/arktouros-apm-server:latest
+docker pull wangminan/arktouros-ui:latest
 mkdir -p /root/arktouros
 cd /root/arktouros || exit
+docker-compose stop arktouros-ui
+docker-compose rm arktouros-ui
 docker-compose stop arktouros-apm-server
 docker-compose rm arktouros-apm-server
 docker-compose up -d
