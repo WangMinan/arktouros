f48dc7d1fd414a9315999f44174ea74ac5bea895
WangMinan <wangminan0811@hotmail.com>
2024-05-02 17:15:25 +0800

    【前端部署】jar方式部署前端，引入armeria服务

---
+++ arktouros-webapp/src/main/java/edu/npu/ApplicationStartUp.java
@@ -0,0 +1,46 @@
+package edu.npu;
+
+import com.linecorp.armeria.common.SessionProtocol;
+import com.linecorp.armeria.server.HttpService;
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.file.FileService;
+import com.linecorp.armeria.server.file.HttpFile;
+import edu.npu.config.PropertiesProvider;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author : [wangminan]
+ * @description : 静态托管http服务主启动类
+ */
+@Slf4j
+public class ApplicationStartUp {
+    public static void main(String[] args) {
+        PropertiesProvider.init();
+        int port = Integer.parseInt(
+                PropertiesProvider.getProperty("server.port", "50052")
+        );
+        log.info("Starting up the application...");
+        final HttpService indexPage =
+                HttpFile
+                        .of(ApplicationStartUp.class.getClassLoader(),
+                                "/public/index.html")
+                        .asService();
+        Server
+                .builder()
+                .port(port, SessionProtocol.HTTP)
+                .serviceUnder("/",
+                        FileService.of(
+                                        ApplicationStartUp.class.getClassLoader(),
+                                        "/public")
+                                .orElse(indexPage))
+                .build()
+                .start()
+                .join();
+        log.info("Application started successfully.");
+        // shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("Shutting down the application...");
+            log.info("Application shutdown successfully.");
+        }));
+    }
+}
