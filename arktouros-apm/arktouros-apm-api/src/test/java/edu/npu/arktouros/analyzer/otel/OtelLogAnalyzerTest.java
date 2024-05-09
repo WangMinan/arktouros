0c92e558f9788c32417fd5c3c6f6935870598a7d
WangMinan <wangminan0811@hotmail.com>
2024-05-09 20:57:42 +0800

    【补UT】

---
+++ arktouros-apm/arktouros-apm-api/src/test/java/edu/npu/arktouros/analyzer/otel/OtelLogAnalyzerTest.java
@@ -0,0 +1,19 @@
+package edu.npu.arktouros.analyzer.otel;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * @author : [wangminan]
+ * @description : {@link OtelLogAnalyzer}
+ */
+@SpringBootTest
+@ExtendWith({SpringExtension.class, MockitoExtension.class})
+@Slf4j
+@Disabled
+public class OtelLogAnalyzerTest {
+}
