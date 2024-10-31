561bf0678a9d758af994e2534ad698ddd189a07d
wangminan <wangminan0811@hotmail.com>
2024-11-01 01:15:52 +0800

    【FIX】修正部分小bug的补丁同步到仓库

--- arktouros-apm/arktouros-apm-model/src/main/java/edu/npu/arktouros/model/vo/SpanTreeNodeVo.java
+++ arktouros-apm/arktouros-apm-model/src/main/java/edu/npu/arktouros/model/vo/SpanTreeNodeVo.java
@@ -1,7 +1,7 @@
 package edu.npu.arktouros.model.vo;
 
-import edu.npu.arktouros.config.PropertiesProvider;
 import edu.npu.arktouros.model.common.PersistentDataConstants;
+import edu.npu.arktouros.model.config.PropertiesProvider;
 import edu.npu.arktouros.model.otel.topology.span.SpanTreeNode;
 import edu.npu.arktouros.model.otel.trace.Span;
 import lombok.Builder;
@@ -22,7 +22,7 @@ public class SpanTreeNodeVo {
     private ItemStyle itemStyle;
     // 我要加这个字段 不然前端看不出来span的实时状态
     private Span span;
-    private int spanTimeout; 
+    private int spanTimeout;
 
     private static final String COLOR_ERROR_RED_RGB = "#FF2700";
     private static final String COLOR_ERROR_YELLOW_RGB = "#FFEE00";
@@ -30,10 +30,10 @@ public class SpanTreeNodeVo {
 
     // 直接递归
     public SpanTreeNodeVo(SpanTreeNode spanTreeNode) {
-        this.spanTimeout = Integer.parseInt(PropertiesProvider.getProperty("sinker.span.timeout","5000")); 
+        this.spanTimeout = Integer.parseInt(PropertiesProvider.getProperty("sinker.span.timeout","5000"));
         this.span = spanTreeNode.getSpan();
         this.name = spanTreeNode.getSpan().getName();
-        if (spanTreeNode.getSpan().getEndTime() == PersistentDataConstants.ERROR_SPAN_END_TIME || 
+        if (spanTreeNode.getSpan().getEndTime() == PersistentDataConstants.ERROR_SPAN_END_TIME ||
             spanTreeNode.getSpan().getEndTime() - spanTreeNode.getSpan().getStartTime() > spanTimeout) {
             // 橙红色
             this.itemStyle = ItemStyle.builder()
